c75d979a2fcbb1298db13140a92c7c15fb034721
diana maina <dianamainachui@gmail.com>
2020-11-11 05:56:25 -0500

    header buttons

--- employeedirectory/src/components/Header/Header.jsx
+++ employeedirectory/src/components/Header/Header.jsx
@@ -1,22 +1,83 @@
+import { Component } from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import EmployeeRows from "./EmployeeRows";
+import SearchBar from "./SearchBar";
+import "../styles/index.css";
+
+class EmployeeTable extends Component {
+  state = {
+    employees: [],
+    search: "",
+  };
+
+  // Calls the API and sets the state as soon as the component mounts
+  componentDidMount = () => {
+    axios
+      .get("https://randomuser.me/api/?results=20&nat=us")
+      .then((response) => {
+        this.setState({ employees: response.data.results });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  // Function to set the state based on the user's input
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // Function to sort the table by employee name
+  sortTableByName = () => {
+    this.setState({
+      employees: this.state.employees.sort((a, b) => {
+        if (a.name.last < b.name.last) {
+          return -1;
+        } else if (a.name.last > b.name.last) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }),
+    });
+  };
+
   
-import React from "react";
-import "../style/header.css";
-
-const Header = (props) => {
-  return (
-    <div className={"jumbotron"}>
-      <h1>Employee Directory</h1>
-      <h4>
-       Enter first name to search.
-      </h4>
-      <input
-        type="text"
-        name="search"
-        placeholder="Search by First Name"
-        onChange={props.handleInput}
-      />
-    </div>
-  );
-};
-
-export default Header;
+  // Renders this element to the page
+  render() {
+    return (
+      <>
+        <SearchBar
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
+        <Table striped bordered hover className="text-center eTable">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>
+                Name
+                <Button variant="link" onClick={this.sortTableByName} size="sm">
+                </Button>
+              </th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>
+                Date of Birth
+                <Button variant="link" onClick={this.sortTableByName} size="sm">
+                    </Button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <EmployeeRows state={this.state} />
+          </tbody>
+        </Table>
+      </>
+    );
+  }
+}
+export default EmployeeTable;
